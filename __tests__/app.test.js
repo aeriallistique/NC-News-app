c9d46bbaf6e88f22048d76563b8ae7c6d4b04329
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-01-27 14:41:28 +0000

    add tested code for GET/api/articles/:article_id endpoint aka task4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,7 +7,6 @@ const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data/index');
 
 /* Set up your beforeEach & afterAll functions here */
-
 beforeEach(() => {
   return seed(data);
 });
@@ -26,21 +25,57 @@ describe("GET /api", () => {
       });
   });
 });
-describe.only("GET /api/topics", () => {
+describe("GET /api/topics", () => {
   test("200 responds with and array of topic objects", () => {
     return request(app)
       .get('/api/topics')
       .expect(200)
       .then((response) => {
-        const result = response._body.topics;
-        expect(Array.isArray(result)).toBe(true);
-        result.forEach(obj => {
-          expect(obj.hasOwnProperty('slug')).toBe(true);
-          expect(obj.hasOwnProperty('description')).toBe(true);
+        const topics = response._body.topics;
+        expect(Array.isArray(topics)).toBe(true);
+        expect(topics.length).not.toBe(0);
+        topics.forEach(topic => {
+          expect(topic.hasOwnProperty('slug')).toBe(true);
+          expect(topic.hasOwnProperty('description')).toBe(true);
         });
+      });
+  });
+});
 
+describe("GET/api/articles/:article_id", () => {
+  test('200 responds with an article object', () => {
+    return request(app)
+      .get('/api/articles/3')
+      .expect(200)
+      .then((response) => {
+        const { article } = response._body;
+        console.log(article);
+        expect(typeof article).toBe('object');
+        expect(Object.keys(article).length).not.toBe(0);
+        expect(article.hasOwnProperty('author')).toBe(true);
+        expect(article.hasOwnProperty('title')).toBe(true);
+        expect(article.hasOwnProperty('body')).toBe(true);
+        expect(article.hasOwnProperty('topic')).toBe(true);
+        expect(article.hasOwnProperty('created_at')).toBe(true);
+        expect(article.hasOwnProperty('votes')).toBe(true);
+        expect(article.hasOwnProperty('article_img_url')).toBe(true);
 
       });
-
+  });
+  test('404 not found, responds with error message article not found', () => {
+    return request(app)
+      .get('/api/articles/3333')
+      .expect(404)
+      .then((response) => {
+        expect(response._body.error).toBe('Article not found');
+      });
+  });
+  test('400 bad request, responds with error message bad request', () => {
+    return request(app)
+      .get('/api/articles/notandID')
+      .expect(400)
+      .then((response) => {
+        expect(response._body.error).toBe('Bad request.');
+      });
   });
 });
