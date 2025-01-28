e1e9771346bee19e767caf38b1c87bdbec7f798c
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-01-28 17:37:31 +0000

    add completed and tested task7 plus refactored code for task6

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,19 +5,12 @@ const db = require('../db/connection');
 const app = require('../app');
 const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data/index');
-const articles = require("../db/data/test-data/articles");
 
 /* Set up your beforeEach & afterAll functions here */
-beforeEach(() => {
-  return seed(data);
-});
+beforeEach(() => seed(data));
+afterAll(() => db.end());
 
-afterAll((done) => {
-  db.end();
-  done();
-});
-
-describe.skip("GET /api", () => {
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
@@ -27,7 +20,8 @@ describe.skip("GET /api", () => {
       });
   });
 });
-describe.skip("GET /api/topics", () => {
+
+describe("GET /api/topics", () => {
   test("200 responds with and array of topic objects", () => {
     return request(app)
       .get('/api/topics')
@@ -44,7 +38,7 @@ describe.skip("GET /api/topics", () => {
   });
 });
 
-describe.skip("GET/api/articles/:article_id", () => {
+describe("GET/api/articles/:article_id", () => {
   test('200 responds with an article object', () => {
     return request(app)
       .get('/api/articles/2')
@@ -71,17 +65,20 @@ describe.skip("GET/api/articles/:article_id", () => {
         expect(response._body.error).toBe('Article not found');
       });
   });
+
+});
+describe('404 not found', () => {
   test('400 bad request, responds with error message bad request', () => {
     return request(app)
-      .get('/api/articles/notandID')
+      .get('/api/articles/Id3')
       .expect(400)
       .then((response) => {
-        expect(response._body.error).toBe('Bad request.');
+        expect(response._body.error).toBe('Not a valid id');
       });
   });
 });
 
-describe.skip("GET /api/articles", () => {
+describe("GET /api/articles", () => {
   test('200 responds with an articles array of objects with correct properties', () => {
     return request(app)
       .get('/api/articles')
@@ -118,27 +115,88 @@ describe.skip("GET /api/articles", () => {
 
 describe("GET /api/articles/:article_id/comments", () => {
   test("200 responds with an array of comments for the given article_id", () => {
-    return request(app).get('/api/articles/9/comments').expect(200).then((response) => {
-      const comments = response._body.comments;
-      expect(comments.length).not.toBe(0);
-      comments.forEach(comment => {
-        expect(comment.hasOwnProperty('comment_id')).toBe(true);
-        expect(comment.hasOwnProperty('votes')).toBe(true);
-        expect(comment.hasOwnProperty('created_at')).toBe(true);
-        expect(comment.hasOwnProperty('author')).toBe(true);
-        expect(comment.hasOwnProperty('body')).toBe(true);
-        expect(comment.hasOwnProperty('article_id')).toBe(true);
+    return request(app)
+      .get('/api/articles/1/comments')
+      .expect(200)
+      .then((response) => {
+        const comments = response._body.comments;
+        expect(comments.length).not.toBe(0);
+        comments.forEach(comment => {
+          expect(comment.hasOwnProperty('comment_id')).toBe(true);
+          expect(comment.hasOwnProperty('votes')).toBe(true);
+          expect(comment.hasOwnProperty('created_at')).toBe(true);
+          expect(comment.hasOwnProperty('author')).toBe(true);
+          expect(comment.hasOwnProperty('body')).toBe(true);
+          expect(comment.hasOwnProperty('article_id')).toBe(true);
+        });
+        expect(comments).toBeSortedBy('created_at', { descending: true, coerce: true });
       });
-      expect(comments).toBeSortedBy('created_at', { descending: true, coerce: false });
-    });
-  }, 10000);
+  });
 
   describe("404 not found", () => {
-    test('responds with 404 and No comment with article_id found when there are no comments with specified ID', () => {
-      return request(app).get('/api/articles/2/comments').expect(404).then((response) => {
-        expect(response._body.error).toBe('No comment with article_id found');
+    test('responds with 404 and Not a valid id  when passing an invalid id', () => {
+      return request(app)
+        .get('/api/articles/myid/comments')
+        .expect(400)
+        .then((response) => {
+          expect(response._body.error).toBe('Not a valid id');
+
+        });
+    });
+  });
+  test('responds with 200 and Not a valid id  when passing a valid id but no articles exist', () => {
+    return request(app)
+      .get('/api/articles/2222/comments')
+      .expect(404)
+      .then((response) => {
+        expect(response._body.error).toBe('Article not found');
 
       });
+  });
+});
+
+
+describe("POST /api/articles/:article_id/comments", () => {
+  test('endpoint should respond with 201 and object of posted comment', () => {
+    const postObj = { username: "icellusedkars", body: "Test comment, test-comment, test, TEST COMMENT, comment" };
+    return request(app).post('/api/articles/9/comments').send(postObj).expect(201).then((response) => {
+      const comment = response._body.comment;
+      console.log(comment);
+
+      expect(typeof comment).toBe('object');
+      expect(comment.author).toEqual(postObj.username);
+      expect(comment.body).toEqual(postObj.body);
+      expect(comment.hasOwnProperty('comment_id')).toBe(true);
+      expect(comment.hasOwnProperty('article_id')).toBe(true);
+      expect(comment.hasOwnProperty('votes')).toBe(true);
+      expect(comment.hasOwnProperty('created_at')).toBe(true);
+
+    });
+  });
+  test("400 endpoint responds with Missing fields in the request body is username or body are missing", () => {
+    const postObj = { body: "Test comment, test-comment, test, TEST COMMENT, comment" };
+    return request(app).post('/api/articles/9/comments').send(postObj).expect(400).then((response) => {
+      expect(response._body.error).toBe('Missing fields in the request body');
+    });
+  });
+  test("400 endpoint responds with Missing fields in the request body is username or body are missing", () => {
+    const postObj = { username: 'icellusedkars' };
+    return request(app).post('/api/articles/9/comments').send(postObj).expect(400).then((response) => {
+      expect(response._body.error).toBe('Missing fields in the request body');
+    });
+  });
+
+  test("400 endpoint rejects with message Not a valid user input when passed a non string username", () => {
+    const postObj = { body: "Test comment, test-comment, test, TEST COMMENT, comment", username: 9 };
+    return request(app).post('/api/articles/9/comments').send(postObj).expect(400).then((error) => {
+      expect(error._body.error).toBe('Not a valid user input');
+    });
+  });
+
+  test("400 endpoint rejects with message Not a valid user input when passed a non string username", () => {
+    const postObj = { body: "Test comment, test-comment, test, TEST COMMENT, comment", username: "icellusedkars" };
+    return request(app).post('/api/articles/999/comments').send(postObj).expect(404).then((error) => {
+      expect(error._body.error).toBe('Article not found');
     });
   });
 });
