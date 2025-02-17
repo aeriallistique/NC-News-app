31f2872dc67be6802615c142f78fbed6e5130dcc
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-02-17 11:22:11 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", apiRouter);
 
 
